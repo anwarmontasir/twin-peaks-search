cc37cde0b7f9adbe407a193afd7943ef2e4baa49
Anwar Montasir <anwarmontasir@gmail.com>
2018-12-06 12:45:45 -0800

    peaks.json

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LiveSearch from './components/live-search';
 import './index.css';
+import peaks from './peaks.json';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<LiveSearch />, document.getElementById('root'));
